823b4d1c2a6f311f3c20f2cc97dc858e2863252e
davique0 <davique@gmail.com>
2022-11-18 19:20:49 -0500

    Search vertically all working and passing test

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,22 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  if (!word) return undefined;
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    } else {
+      for (let y = 0; y < letters[0].length; y++) {
+        const verticalJoin = [];
+        for (let x = 0; x < letters.length; x++) {
+          verticalJoin.push(letters[x][y]);
+        }
+        let verticalWord = verticalJoin.join('');
+        if (verticalWord.includes(word)) return true;
+      }
     }
-}
+  }
+  return false;
+};
+// Made by: @brotherludi and @davique0
 
-module.exports = wordSearch
+module.exports = wordSearch;
